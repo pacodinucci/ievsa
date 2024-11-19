a345b491c7dd5b33d7a22c006d898be689c62002
pacodinucci <franciscoldinucci@gmail.com>
2024-11-18 21:24:12 -0300

    background color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { Menu } from "lucide-react"; // Importa el ícono de Lucide React
 
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState(false);
@@ -24,14 +25,22 @@ const Navbar = () => {
 
   return (
     <div
-      className={`fixed flex justify-between items-center h-[12vh] w-full z-50 px-12 bg-[#CAD29D] bg-opacity-30 backdrop-blur-lg transition-opacity duration-1000 ${
+      className={`fixed flex justify-between items-center h-[12vh] w-full z-50 px-6 md:px-12 bg-[#CAD29D] bg-opacity-30 backdrop-blur-lg transition-opacity duration-1000 ${
         showNavbar ? "opacity-100" : "opacity-0"
       }`}
     >
+      {/* Logo */}
       <div>
         <Image src="/logoievsa2.png" alt="logo ievsa" width={80} height={0} />
       </div>
-      <div>Menu</div>
+
+      {/* Menu */}
+      <div className="hidden md:block">Menu</div>
+
+      {/* Ícono de menú en pantallas pequeñas */}
+      <div className="block md:hidden">
+        <Menu size={32} color="#4B634E" />
+      </div>
     </div>
   );
 };
