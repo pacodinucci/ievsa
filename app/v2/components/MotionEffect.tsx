a4c3ae6a9967cfb41489350a74dc4f04cbf64924
pacodinucci <franciscoldinucci@gmail.com>
2024-12-03 11:45:26 -0300

    image fix motion effect

--- app/v2/components/MotionEffect.tsx
+++ app/v2/components/MotionEffect.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef } from "react";
-import Image from "next/image";
 import Lenis from "lenis";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
