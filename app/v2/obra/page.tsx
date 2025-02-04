4152168590e2bf07182be8d29bf7eeca82b79b98
pacodinucci <franciscoldinucci@gmail.com>
2025-02-04 18:12:59 -0300

    footer update

--- app/v2/obra/page.tsx
+++ app/v2/obra/page.tsx
@@ -2,12 +2,16 @@
 
 import React from "react";
 import Phases from "../components/Phases";
+import Footer from "../components/Footer";
 
 const ObraPage = () => {
   return (
-    <div className="bg-[#F2F2F2] border border-[#F2F2F2]">
-      <Phases />
-    </div>
+    <>
+      <div className="bg-[#F2F2F2] border border-[#F2F2F2]">
+        <Phases />
+      </div>
+      <Footer />
+    </>
   );
 };
 
--- app/v2/postventa/page.tsx
+++ app/v2/postventa/page.tsx
@@ -4,30 +4,34 @@ import Image from "next/image";
 import React from "react";
 import SectionTitle from "../components/SectionTitle";
 import { useRouter } from "next/navigation";
+import Footer from "../components/Footer";
 
 const PostventaPage = () => {
   const router = useRouter();
 
   return (
-    <div className="bg-[#F2F2F2] border border-[#F2F2F2] min-h-screen">
-      <div className="pl-24 pt-12">
-        <Image
-          src="/logoievsa2.png"
-          alt="logo ievsa"
-          width={200}
-          height={100}
-          className="cursor-pointer"
-          onClick={() => router.push("/v2")}
+    <>
+      <div className="bg-[#F2F2F2] border border-[#F2F2F2] min-h-screen">
+        <div className="pl-24 pt-12">
+          <Image
+            src="/logoievsa2.png"
+            alt="logo ievsa"
+            width={200}
+            height={100}
+            className="cursor-pointer"
+            onClick={() => router.push("/v2")}
+          />
+        </div>
+        <SectionTitle
+          title="Post-Venta"
+          className="uppercase text-green-950 mt-16 mb-12 px-12"
         />
+        <div className="MyriadText w-[50%] px-24">
+          Aca va el contenido de Post-Venta.
+        </div>
       </div>
-      <SectionTitle
-        title="Post-Venta"
-        className="uppercase text-green-950 mt-16 mb-12 px-12"
-      />
-      <div className="MyriadText w-[50%] px-24">
-        Aca va el contenido de Post-Venta.
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
